0f90fb4b4617a5f554611b268acdefefed6f27b1
ArhaanAli04 <arhaan.ali2004@gmail.com>
2024-07-30 23:58:43 +0530

    react

---
+++ my-react-app/src/Card.jsx
@@ -0,0 +1,13 @@
+
+function Card(){
+    return(
+        <div className="card">
+            <img></img>
+            <h1>Arhaan Ali</h1>
+            <p>I am learning Reacgt at the moment</p>
+
+        </div>
+
+    );
+}
+export default Card
